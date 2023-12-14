e30ca2a7e2b7ade812f5c8b2c9d890ffa3e6eb93
Jake Pelter <jake.pelter@gmail.com>
2023-12-14 16:52:45 -0500

    python flask api

---
+++ python-flask-api/src/server.py
@@ -0,0 +1,30 @@
+# STL IMPORTS
+
+# EXT IMPORTS
+from flask import Flask
+from waitress import serve
+
+# FLASK INIT
+app = Flask(__name__)
+
+#########################
+##### SERVER ROUTES #####
+#########################
+@app.route("/", methods=['GET'])
+def hello_world():
+    print("Someone visited!")
+    return "<p>Hello world and all who inhabit it!</p>"
+
+@app.route("/api", methods=['GET'])
+def hello_world():
+    print("Someone viffffffasdsited!")
+    return "<p>Hello world and all fffwho inhabit it!</p>"
+
+@app.route("/api/test", methods=['GET'])
+def hello_world():
+    print("S123123123omeone visited!")
+    return "<p>Hello world and all fffwho inhabitzz it!</p>"
+
+if __name__ == '__main__':
+    print("Starting server!")
+    serve(app, host='0.0.0.0', port=8080)
