d77e8ca8bee8fcc8c7ff88314b7024d8f877c36d
Jake Pelter <jake.pelter@gmail.com>
2023-12-15 19:34:43 -0500

    remove extra ,

--- liquibase/src/scripts/000-users-auth.sql
+++ liquibase/src/scripts/000-users-auth.sql
@@ -1,4 +1,4 @@
 CREATE TABLE USER (
     email VARCHAR(100) PRIMARY KEY,
-    creation_timestamp TIMESTAMPTZ DEFAULT current_timestamp,
+    creation_timestamp TIMESTAMPTZ DEFAULT current_timestamp
 );
