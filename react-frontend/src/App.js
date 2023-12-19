d7ba59b6de49f6839755ff6e8dea6e452fdd061e
Jake Pelter <jake.pelter@gmail.com>
2023-12-19 08:07:36 -0500

    frontend skeleton updates

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import NavBar from './NavBar';
+import Home from './Home';
+import Services from './Services';
+import Appointments from './Appointments';
+import Chat from './Chat';
+// import Login from './Login';
 
 function App() {
+  const [ Authenticated, setAuthenticated ] = useState(false);
+
+  useEffect(() => {
+    // Simulated authentication check on app load
+    // Replace this with your actual API call to check authentication
+    async function checkAuth() {
+      try {
+        const response = await fetch(process.env.REACT_APP_AUTH_API);
+        if (response.status === 200) {
+          setAuthenticated(true);
+        } else {
+          setAuthenticated(false);
+        }
+      } catch (error) {
+        setAuthenticated(false);
+      }
+    }
+    checkAuth();
+  }, [setAuthenticated]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar Authenticated={Authenticated} setAuthenticated={setAuthenticated}/>
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          {Authenticated ? (
+            <>
+              <Route path="/appointments" component={Appointments} />
+              <Route path="/chat" component={Chat} />
+            </>
+          ) : (
+            <Route path="/login" component={Chat} />
+          )}
+        </Routes>
+    </Router>
   );
 }
 
---
+++ react-frontend/src/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+import { Link } from 'react-router-dom';
+
+export default function ButtonAppBar({Authenticated, setAuthenticated}) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Link to="/">
+            <Typography variant="h6" component="div">
+                Home
+            </Typography>
+          </Link>
+          <Link to="/services">
+            <Typography variant="h6" component="div">
+                Services
+            </Typography>
+          </Link>
+          {Authenticated ? (
+            <>
+                <Link to="/appointments">
+                    <Typography variant="h6" component="div">
+                        Appointments
+                    </Typography>
+                </Link>
+                <Link to="/chat">
+                    <Typography variant="h6" component="div" >
+                        Chat
+                    </Typography>
+                </Link>
+                <div sx={{ flexGrow: 1 }}></div>
+                <Button >Logout</Button>
+            </>
+            ) : (
+            <Button>Login</Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
