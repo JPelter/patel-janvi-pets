996bc44bc7adbea04f19d2c0b7937330864da9a9
Jake Pelter <jake.pelter@gmail.com>
2023-12-19 19:36:38 -0500

    no box?

--- react-frontend/src/NavBar.js
+++ react-frontend/src/NavBar.js
@@ -2,48 +2,33 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 
 import { Link } from 'react-router-dom';
 
 export default function ButtonAppBar({Authenticated, setAuthenticated}) {
   return (
-    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Link to="/">
-            <Typography variant="h6" component="div">
-                Home
-            </Typography>
+            HOME
           </Link>
           <Link to="/services">
-            <Typography variant="h6" component="div">
-                Services
-            </Typography>
+            SERVICES
           </Link>
           {Authenticated && (
             <>
               <Link to="/appointments">
-                  <Typography variant="h6" component="div">
-                      Appointments
-                  </Typography>
+                APPOINTMENTS
               </Link>
               <Link to="/chat">
-                  <Typography variant="h6" component="div">
-                      Chat
-                  </Typography>
+                CHAT
               </Link>
             </>
           )}
-          <div sx={{ flexGrow: 1 }}></div>
           <Link to="/account">
-            <Typography variant="h6" component="div">
-                Account
-            </Typography>
+            ACCOUNT
           </Link>
         </Toolbar>
       </AppBar>
-    </Box>
   );
 }
