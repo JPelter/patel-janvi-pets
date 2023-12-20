c417a83c3ff0313f7dfe99fac419dcad81fe52f3
Jake Pelter <jake.pelter@gmail.com>
2023-12-19 19:48:23 -0500

    nav bar buttons

--- react-frontend/src/NavBar.js
+++ react-frontend/src/NavBar.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import { NavLink } from 'react-router-dom';
 
-import { Link } from 'react-router-dom';
+import './NavBar.css';
 
-export default function ButtonAppBar({Authenticated, setAuthenticated}) {
+export default function ButtonAppBar({ authenticated, setAuthenticated }) {
+
   return (
-      <AppBar position="static">
-        <Toolbar>
-          <Link to="/">
-            HOME
-          </Link>
-          <Link to="/services">
-            SERVICES
-          </Link>
-          {Authenticated && (
-            <>
-              <Link to="/appointments">
-                APPOINTMENTS
-              </Link>
-              <Link to="/chat">
-                CHAT
-              </Link>
-            </>
-          )}
-          <Link to="/account">
-            ACCOUNT
-          </Link>
-        </Toolbar>
-      </AppBar>
+    <AppBar position="static" style={{ backgroundColor: '#333' }}>
+      <Toolbar>
+        <Button component={NavLink} to="/" exact style={{ marginRight: '10px' }} variant="contained" color="primary" activeClassName="active">
+          HOME
+        </Button>
+        <Button component={NavLink} to="/services" style={{ marginRight: '10px' }} variant="contained" color="primary" activeClassName="active">
+          SERVICES
+        </Button>
+        {authenticated && (
+          <>
+            <Button component={NavLink} to="/appointments" style={{ marginRight: '10px' }} variant="contained" color="primary" activeClassName="active">
+              APPOINTMENTS
+            </Button>
+            <Button component={NavLink} to="/chat" style={{ marginRight: '10px' }} variant="contained" color="primary" activeClassName="active">
+              CHAT
+            </Button>
+          </>
+        )}
+        <Button component={NavLink} to="/account" variant="contained" color="primary" activeClassName="active">
+          ACCOUNT
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
