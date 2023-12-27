9629fc861021fca8862e0acc61ae56c44786be48
Jake Pelter <jake.pelter@gmail.com>
2023-12-27 12:14:36 -0500

    event icon

--- react-frontend/src/Appointments.js
+++ react-frontend/src/Appointments.js
@@ -1,14 +1,13 @@
 import { Paper, Stack, Typography, Link  } from '@mui/material';
-import PetsIcon from '@mui/icons-material/Pets';
+import EventIcon from '@mui/icons-material/Event';
 import { Link as RouterLink } from 'react-router-dom';
 
 function Appointments(props) {
     return (
         <div style={{ textAlign: 'center' }}>
             <Paper elevation={3} style={{ padding: '20px', margin: '20px'}} component={Stack} direction="row" justifyContent="center" alignItems="center" spacing={2}>
-                <PetsIcon sx={{ fontSize: '3.5rem' }} />
+                <EventIcon sx={{ fontSize: '3.5rem' }} />
                 <Typography variant="h3" sx={{ fontFamily: 'Dancing Script'}}>Appointments!</Typography>
-                <PetsIcon sx={{ fontSize: '3.5rem' }} />
             </Paper>
         </div>
     );
