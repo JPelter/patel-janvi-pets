7729442409d4be3f100e7f98c9addf4362e9f15c
Jake Pelter <jake.pelter@gmail.com>
2023-12-27 08:55:12 -0500

    fallback font

--- react-frontend/src/Appointments.js
+++ react-frontend/src/Appointments.js
@@ -7,7 +7,7 @@ function Appointments(props) {
         <div style={{ textAlign: 'center' }}>
             <Paper elevation={3} style={{ padding: '20px', margin: '20px'}} component={Stack} direction="row" justifyContent="center" alignItems="center" spacing={2}>
                 <PetsIcon sx={{ fontSize: '3.5rem' }} /> {/* Adjust the fontSize */}
-                <Typography variant="h3" sx={{ fontFamily: 'Brush Script MT'}}>Appointments!</Typography> {/* Set the fontFamily */}
+                <Typography variant="h3" sx={{ fontFamily: 'Brush Script MT,DancingScript'}}>Appointments!</Typography> {/* Set the fontFamily */}
             </Paper>
         </div>
     );
